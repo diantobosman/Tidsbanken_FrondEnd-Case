9d264f4d5a33d455e2e610ed4ed9aee6ccf81c31
Dianto <diantobosman1@gmail.com>
2022-04-08 19:09:07 +0200

    Feat: logged in user connected with keycloak

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -1,8 +1,16 @@
-export interface Employee {
-    id: number;
-    email: string;
-    name: String;
-    profilePicture: string; 
-    adminId: number;
-    isAdmin: boolean;
+export interface Employee{
+    preferred_username: string,
+    fullName: string,
+    firstName:string,
+    email: string,
+    lastName: string
 }
+
+// export interface Employee {
+//     id: number;
+//     email: string;
+//     username: String;
+//     profilePicture: string; 
+//     adminId: number;
+//     isAdmin: boolean;
+// }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,18 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { map, Observable, of, switchMap } from 'rxjs';
+import { StorageKeys } from '../enums/storage-keys.enum';
 import { Employee } from '../models/employee.model';
-
-
-// Create a testuser instead of fetching the user from the API
-const testUser: Employee= {
-  id: 1,
-  email: 'foo@gmail.com',
-  name: 'foo',
-  profilePicture: 'test.png', 
-  adminId: 2,
-  isAdmin: true
-}
+import { StorageUtil } from '../utils/storage.util';
 
 @Injectable({
   providedIn: 'root'
@@ -20,43 +12,69 @@ const testUser: Employee= {
 
 
 export class LoginService {
+  private _employee: Employee = {
+    preferred_username: "none",
+    fullName: "none",
+    firstName: "none",
+    email: "none@none",
+    lastName: "none"
+}
 
-  constructor(private readonly http: HttpClient) { 
+  constructor(
+    private readonly router: Router,
+    private readonly http: HttpClient) { 
+    
   }
+
+  get employee(){
+    return this._employee
+  }
+
+  set employee(employee:Employee){
+    this._employee = employee
+  }
+
 
   // Login
-  public login(email: string): Employee | undefined {
-    if ( email === testUser.email) {
-      
-      return testUser
-    }
-    else {
-      console.log("This user does not exist.")
-      return undefined
-    }
+  public login(username: string, password: string) {
 
-    // return this.checkEmail(email)
-    // .pipe(
-    //   switchMap((testUser: User | undefined) => {
-    //     if (testUser === undefined) {
-    //       return console.log("This user does not exist.")
-    //     }
-    //     return of(testUser)
-    //   })
-    // )
+    const headers = new HttpHeaders ({
+      "Content-Type": "application/x-www-form-urlencoded"
+    })
+
+    console.log(username)
+    console.log(password)
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("client_id", "tidsbanken-frontend");
+    urlencoded.append("username", username);
+    urlencoded.append("password", password);
+    urlencoded.append("grant_type", "password");
+
+    this.http.post<any>('https://keycloak-tidsbanken-case.herokuapp.com/auth/realms/tidsbankencase/protocol/openid-connect/token', urlencoded, { headers })
+    .pipe(
+        map(kcResult => {
+          return {
+            ...kcResult,
+            decodedToken: JSON.parse(atob(kcResult.access_token.split('.')[1]))
+           }
+         })
+      )
+      .subscribe({
+       next: (result)=>{
+        sessionStorage.setItem('authToken', result.access_token)
+         this._employee = {
+             preferred_username: result.decodedToken.preferred_username,
+             fullName: result.decodedToken.name,
+             firstName: result.decodedToken.given_name,
+             lastName:result.decodedToken.family_name,
+             email:result.decodedToken.email
+         }
+         console.log(this._employee)
+         StorageUtil.storageSave<Employee>(StorageKeys.Employee, this._employee)
+         this.router.navigateByUrl('/calendar')
+         
+       },
+       error:(error) => {console.log("hello", error)}
+     })
   }
-
-
-  // Check if the user exists
-  // private checkEmail(email: string): Observable<User | undefined> {
-    
-  //   Below should be implemented with the API
-  //   return this.http.get<User[]>()
-  //     .pipe(
-  //       map((response: User[]) => response.pop())
-  //     )
-
-  //   testUser.email = email;
-  //   return testUser?
-  // }
 }
