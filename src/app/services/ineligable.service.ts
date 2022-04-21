8712df9bb477fcbc0488cbf00e4f1235a8689994
savannah-borst <savannah.borst@gmail.com>
2022-04-21 14:37:09 +0200

    feat: model + service

---
+++ src/app/services/ineligable.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient, HttpErrorResponse, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { finalize, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { StorageKeys } from '../enums/storage-keys.enum';
+import { Ineligable } from '../models/ineligable.model';
+import { StorageUtil } from '../utils/storage.util';
+
+const {APIURL} = environment;
+const token = StorageUtil.storageRead(StorageKeys.AuthKey);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IneligableService {
+
+  private _ineligableById?: Ineligable;
+  private _isLoading: boolean = false;
+  private _error: any = '';
+
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  //-- Get all Ineligable periods
+  public getIneligable(): Observable<Ineligable[]> {
+    
+    const headers = new HttpHeaders ({
+      "Accept": "*/*",
+      "Authorization": `Bearer ${token}`
+    })
+
+    this._isLoading = true;
+
+    return this.http.get<Ineligable[]>(`${APIURL}ineligibleperiod/all`, {headers})
+    .pipe(
+      map((response: any) => response),
+      finalize(() => this._isLoading = false)
+    )
+  }
+
+  //-- Get Ineligable period by ID
+  public getIneligableById(ineligableId: number): void {
+
+    const headers = new HttpHeaders ({
+      "Accept": "*/*",
+      "Authorization": `Bearer ${token}`
+    })
+
+    this.http.get<Ineligable>(`${APIURL}ineligibleperiod/${ineligableId}`, {headers})
+    .pipe(
+      map((response:  any) => response),
+    )
+    .subscribe({
+      next: (ineligable: Ineligable) => {
+        this._ineligableById = ineligable;
+      },
+      error: (error: HttpErrorResponse) => {
+        this._error = error.message;
+      }
+    })
+  }
+
+
+  //-- Delete Ineligable period by ID
+  public deleteIneligable(ineligableId: number): void {
+
+    const headers = new HttpHeaders ({
+      "Accept": "*/*",
+      "Authorization": `Bearer ${token}`
+    })
+
+    this.http.delete<Ineligable>(`${APIURL}ineligibleperiod/delete/${ineligableId}`, {headers})
+    .subscribe({
+      next: () => {
+        //alert
+        console.log(`ineligable period with id ${ineligableId} deleted`);
+      },
+      error: (error: HttpErrorResponse) => {
+        this._error = error.message;
+      }
+    })
+  }
+
+}
