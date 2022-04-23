77f16ecac44ce6e103e0967e63d9d0c5d34ede4b
Dianto <diantobosman1@gmail.com>
2022-04-23 13:10:57 +0200

    Request now dissapears from the screen if approved or denied

--- src/app/components/admin-pending-requests/admin-pending-requests.component.ts
+++ src/app/components/admin-pending-requests/admin-pending-requests.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { identifierName } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { StorageKeys } from 'src/app/enums/storage-keys.enum';
+import { AllEmployeesService } from 'src/app/services/all-employees.service';
 import { StorageUtil } from 'src/app/utils/storage.util';
 import { environment } from 'src/environments/environment';
 
@@ -10,32 +11,21 @@ import { environment } from 'src/environments/environment';
   templateUrl: './admin-pending-requests.component.html',
   styleUrls: ['./admin-pending-requests.component.css']
 })
-export class AdminPendingRequestsComponent implements OnInit {
+
+export class AdminPendingRequestsComponent {
+
+  //-- Binding the fetched requests from the admin component
   @Input() requests: any[] = [];
-  public allTitles: any;
-  public comments: any;
 
   constructor(
     private readonly http: HttpClient
-  ) {
-    
-   }
+  )
+  {}
 
-  ngOnInit(): void {
-    this.onlyPendingRequests()
-  }
-
-  public showRequests() {
-    this.onlyPendingRequests()
-  }
-
-  public onlyPendingRequests() {
-    this.allTitles = this.requests.map((element: { title: any }) => element.title)
-    this.comments = this.requests.map((element: { comments: any }) => element.comments)
-  }
-
+  //-- This function is called when the approve button is clicked
   public approveButton(requestId: number) {
     
+    //-- Get the employee token from localstorage
     const employeeToken = StorageUtil.storageRead(StorageKeys.AuthKey);
 
     //-- Define the headers
@@ -51,19 +41,26 @@ export class AdminPendingRequestsComponent implements OnInit {
       "status": "APPROVED"
     }
 
+    //-- Fetch request
     return this.http.patch<any>(environment.APIURL + `vacation_request/update/` + requestId, body, {headers})
     .subscribe({
       next: (result)=>{
-        console.log(result)
+
+        //-- Find the index of the request
+        let objIndex = this.requests.findIndex((obj => obj.requestId === result.requestId));
+
+        //-- Change that index using the result of the fetch
+        this.requests[objIndex] = JSON.stringify(result)
+
       },
       error:(error)=> {console.log("Post API not succesfull: " + error)}
     })
   }
 
-
-
+  //-- This function is called when the deny button is clicked
   public denyButton(requestId: number) {
 
+    //-- Get the employee token from localstorage
     const employeeToken = StorageUtil.storageRead(StorageKeys.AuthKey);
 
     //-- Define the headers
@@ -79,13 +76,19 @@ export class AdminPendingRequestsComponent implements OnInit {
       "status": "DENIED"
     }
 
+    //-- Fetch request
     return this.http.patch<any>(environment.APIURL + `vacation_request/update/` + requestId, body, {headers})
     .subscribe({
       next: (result)=>{
-        console.log(result)
+        
+        //-- Find the index of the request
+        let objIndex = this.requests.findIndex((obj => obj.requestId === result.requestId));
+
+        //-- Change that index using the result of the fetch
+        this.requests[objIndex] = JSON.stringify(result)
+        
       },
       error:(error)=> {console.log("Post API not succesfull: " + error)}
     })
   }
-  
 }
