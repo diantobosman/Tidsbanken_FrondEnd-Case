5158137ea586a338b59ce6a58ec87ddce796c34f
Dianto <diantobosman1@gmail.com>
2022-04-26 17:18:37 +0200

    Fix: user now always registered. Fixed the bug

--- src/app/components/new-user-dialog/new-user-dialog.component.ts
+++ src/app/components/new-user-dialog/new-user-dialog.component.ts
@@ -37,8 +37,8 @@ export class NewUserDialogComponent {
         next: async () => {
 
           console.log("Registering the user in keycloak...")
-          await new Promise(resolve => setTimeout(resolve, 3000));
-          this.getUserAndRegister(firstName, lastName, email)
+          await new Promise(resolve => setTimeout(resolve, 500));
+          this.getUserAndRegister(firstName, lastName, email, username)
         },
         error: (error) => {
           console.log("Error while registering in keycloak:" + error)
@@ -46,13 +46,15 @@ export class NewUserDialogComponent {
       })
     }
   
-    public async getUserAndRegister(firstName: string, lastName: string, email: string) {
+    public async getUserAndRegister(firstName: string, lastName: string, email: string, username: string) {
       this.registerService.getUserByAnyKeycloak()
         .subscribe({
           next: async (result) => {
             console.log("Registered succesfully... Register in the API now...")
             //-- Get the id of the employee and then register in the API
-            const idNewEmployee = FunctionUtil.filterByValue(result, email)[0].id
+
+            const idNewEmployee = FunctionUtil.filterByValue(result, username)[0].id
+            
 
             this.registerService.registerAPI(idNewEmployee, firstName, lastName, email)
             
